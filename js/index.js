47b341cb868a13df2a61e20e601e77559017f2ce
sexybell <nguyenhaid98@yahoo,cin>
2023-05-23 21:56:23 +0700

    Bai tap buoi 7

---
+++ js/index.js
@@ -0,0 +1,27 @@
+//Bai 1
+const name1 = prompt('Nhập tên của bạn');
+document.getElementById("bai1").innerText = name1;
+// console.log(name);
+
+//Bai 2
+const name = prompt('Nhập tên của bạn');
+document.getElementById("bai2").innerText = name.toUpperCase();
+
+//Bai 3
+const firstName = prompt('Nhập họ của bạn')
+const lastName = prompt('Nhập tên của bạn')
+document.getElementById("bai3").innerText = firstName + ' ' + lastName;
+
+
+//Bai 4
+const firstNumber = prompt('Nhập số đầu tiên');
+const secondNumber = prompt('Nhập số thứ hai');
+document.getElementById("bai4").innerText = 'Tổng là ' + Number(firstNumber)+Number(secondNumber);
+
+//Bai 5
+
+const number = prompt('Nhập số cần tính');
+document.getElementById("bai5").innerText = number % 2;
+
+
+
